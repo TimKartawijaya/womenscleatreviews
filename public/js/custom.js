ac490a7926e6f5b0d984b22cadeb503ad7c401e4
TimKartawijaya <tim.kartawijaya@gmail.com>
2018-03-22 01:24:21 -0500

    pre-vue cli

---
+++ public/js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".story-content").hide();
+    $(".show_hide").on("click", function () {
+        var txt = $(".story-content").is(':visible') ? 'Read More' : 'Read Less';
+        $(".show_hide").text(txt);
+        $(this).next('.story-content').slideToggle(200);
+    });
+});
